8bfa276c73556fa4a498f33fb62c5013c6e4db03
everma-cmu <everma@cmu.edu>
2016-05-03 22:02:22 -0400

    Fix?

--- Release/src/GraphGen_notSorted_Cuda.d
+++
@@ -1,4 +0,0 @@
-src/GraphGen_notSorted_Cuda.d: ../src/GraphGen_notSorted_Cuda.cu ../src/GraphGen_notSorted_Cuda.h
-
-../src/GraphGen_notSorted_Cuda.h:
-
--- Release/src/PaRMAT.d
+++ Release/src/PaRMAT.d
@@ -1,6 +1,9 @@
-src/PaRMAT.d: ../src/PaRMAT.cpp ../src/GraphGen_sorted.hpp \
- ../src/GraphGen_notSorted.hpp ../src/utils.hpp ../src/Square.hpp \
- ../src/Edge.hpp ../src/internal_config.hpp
+src/PaRMAT.d: ../src/PaRMAT.cpp ../src/GraphGen_notSorted_Cuda.h \
+ ../src/GraphGen_sorted.hpp ../src/GraphGen_notSorted.hpp \
+ ../src/utils.hpp ../src/Square.hpp ../src/Edge.hpp \
+ ../src/internal_config.hpp
+
+../src/GraphGen_notSorted_Cuda.h:
 
 ../src/GraphGen_sorted.hpp:
 
